6fd9b7346709be55460d5627e15f3081b591ca71
zguth <zoltan.guth@doctusoft.com>
2013-07-05 15:37:50 +0200

    fix primitive types handling

--- src/core/lombok/core/handlers/AttributeHandler.java
+++ src/core/lombok/core/handlers/AttributeHandler.java
@@ -3,6 +3,10 @@ package lombok.core.handlers;
 import static lombok.ast.AST.FieldDecl;
 import static lombok.ast.AST.Name;
 import static lombok.ast.AST.Type;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import lombok.RequiredArgsConstructor;
 import lombok.ast.AST;
 import lombok.ast.Call;
@@ -22,7 +26,22 @@ public final class AttributeHandler<TYPE_TYPE extends IType<METHOD_TYPE, ?, ?, ?
 	private final METHOD_TYPE method;
 	private final FIELD_TYPE field;
 	private final DiagnosticsReceiver diagnosticsReceiver;
-
+	
+	private static final Map<String,String> primitives;
+	
+	static {
+		primitives = new HashMap<String,String>(9);
+		primitives.put("boolean", "Boolean");
+		primitives.put("byte", "Byte");
+		primitives.put("char", "Character");
+		primitives.put("double", "Double");
+		primitives.put("float", "Float");
+		primitives.put("int", "Integer");
+		primitives.put("long", "Long");
+		primitives.put("short", "Short");
+		primitives.put("void", "Void");
+	}
+	
 	public void handle() {
 		
 		if( field != null ) {
@@ -60,6 +79,8 @@ public final class AttributeHandler<TYPE_TYPE extends IType<METHOD_TYPE, ?, ?, ?
 		injectAttribute(type, returnType, attributeName);
 	}
 	
+	
+	
 	private void injectAttribute( final TYPE_TYPE type, final TypeRef attributeType, final String attributeName) {
 		final String typeName = attributeType.getTypeName();
 		final Call createAttribute = new Call( Name( Attributes.class), "of" )
@@ -67,20 +88,23 @@ public final class AttributeHandler<TYPE_TYPE extends IType<METHOD_TYPE, ?, ?, ?
 			.withArgument(new StringLiteral(attributeName))
 			.withArgument(AST.ClassLiteral(typeName));
 		
-		final char firstChar = typeName.charAt(0);
-		final String attributeTypeName;
-		if( Character.isLowerCase(firstChar) ) { // Primitive class
-			attributeTypeName = Character.toUpperCase(firstChar) + typeName.substring(1); 
-		} 
-		else {
-			attributeTypeName = typeName;
-		}
 		final TypeRef attributeTypeRef = Type(Attribute.class)
 				.withTypeArgument(Type(type.qualifiedName()))
-				.withTypeArgument(Type(attributeTypeName));
+				.withTypeArgument(Type(getAttributeTypeName(typeName)));
 		
 		type.editor().injectField( FieldDecl(attributeTypeRef, "_" + attributeName)
 				.withInitialization(createAttribute)
 				.makeFinal().makeStatic().makePublic());
+	}
+	
+	private String getAttributeTypeName( final String typeName) {
+		final char firstChar = typeName.charAt(0);
+		if( Character.isLowerCase(firstChar) ) {
+			final String attributeTypeName = primitives.get(typeName);
+			if( attributeTypeName != null) {
+				return attributeTypeName;
+			}
+		}
+		return typeName;
 	}
 }
