fe8ec15d2fd6749062fd6f5fd07e563cb51b1eb1
gabor-farkas <gabor.farkas@doctusoft.com>
2013-11-05 15:14:50 +0100

    deprecated PropertyAttribute.java

--- src/core/lombok/PropertyAttribute.java
+++ src/core/lombok/PropertyAttribute.java
@@ -7,10 +7,12 @@ import java.lang.annotation.Target;
 
 /**
  * Creates an inline com.doctusoft.common.core.bean.Attribute implementation that relies on invoking the propiate getter and setter.
- * The generated code compiles with GWT, becuase it doesn't rely on reflection through apache-common-beanutils 
+ * The generated code compiles with GWT, becuase it doesn't rely on reflection through apache-common-beanutils
+ * @deprecated this was a temporal fix, now you can use the normal {@link Attribute} annotation in GWT too. 
  */
 @Target({ElementType.METHOD, ElementType.FIELD})
 @Retention(RetentionPolicy.SOURCE)
+@Deprecated
 public @interface PropertyAttribute {
 
 }
